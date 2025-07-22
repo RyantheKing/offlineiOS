57d16e51fc12dbab8ebc5615f5aaff490c19d2f0
Ryan King <ryan@familyking.org>
2025-07-22 15:01:30 -0700

    fix sw.js call

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if (!navigator.serviceWorker.controller) {
-    navigator.serviceWorker.register("/sw.js").then(function(reg) {
+    navigator.serviceWorker.register("/offlineiOS/sw.js").then(function(reg) {
         console.log("Service worker has been registered for the scope: " + reg.scope);
     });
 }
