f809936f1be990acd8014a713a6e29be7981963a
Ryan King <ryan@familyking.org>
2025-07-22 14:47:23 -0700

    geo update

--- app.js
+++ app.js
@@ -3,3 +3,22 @@ if (!navigator.serviceWorker.controller) {
         console.log("Service worker has been registered for the scope: " + reg.scope);
     });
 }
+
+const test = document.getElementById('status');
+const output = document.getElementById('output');
+if ('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude, accuracy } = position.coords;
+        test.textContent = 'Location found:';
+        output.textContent = 
+            `Latitude:  ${latitude}\n` +
+            `Longitude: ${longitude}\n` +
+            `Accuracy:  ±${accuracy} meters`;
+    }, {
+        enableHighAccuracy: true,
+        maximumAge: 0,
+        timeout: 10000
+    });
+} else {
+    test.textContent = 'Geolocation is not supported by your device.';
+}
